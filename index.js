f067efc979ee68c2744044067ce248ec3a320673
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-25 19:25:37 +0500

    send files to client

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-app.set('view engine', 'pug')
 
-app.get('/about', (req, res)=>{
-    res.render('about', {name: 'fahad'})
+app.get('/download', (req, res)=>{
+    res.download('./pfp.jpeg.jfif', 'azmeer.jpg', (err)=>{
+        if(err)
+            console.log(err);
+        else
+        return
+            
+    })
 })
 
 app.listen(3000, ()=>{console.log('Server Started');
