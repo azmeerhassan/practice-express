9484205298db0c140859e6215ae96b84956d1da3
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-29 19:04:35 +0500

    sanitize inputs

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express')
-const { check, validationResult } = require('express-validator')
 const app = express()
 
 app.use(express.json())
 
 app.post('/form',[
-    check('name').isLength({ min:3 }).withMessage('Must be atleast 3 characters'),
-    check('email').custom(email=>{
-        if(alreadyHaveEmail(email)){
-            return Promise.reject('Email already exists');
-            
-        }}),
-    check('age').isNumeric().withMessage('Must be a positive number')
+    check('name').isLength({ min:3 }).trim().escape(),
+    check('email').isEmail().normalizeEmail(),
+    check('age').isNumeric().trim().escape()
 ], (req, res)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty())
-        return res.status(422).json({errors: errors.array()})
 
     const name = req.body.name
     const email = req.body.email
